a0d0db7732e2f5477bbec0bc3596b96babf4fd38
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:33:18 +0800

    Created ElectionModel.

---
+++ ElectionSystem/Facades/ElectionModel.cs
@@ -0,0 +1,36 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class ElectionModel : BaseInpc
+    {
+        private readonly Election _election;
+
+        public ElectionModel(Election election)
+        {
+            _election = election;
+        }
+
+        public int Id { get => _election.Id; }
+
+        public string Name
+        {
+            get => _election.Name;
+            set
+            {
+                _election.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsOpen
+        {
+            get => _election.IsOpen;
+            set
+            {
+                _election.IsOpen = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
