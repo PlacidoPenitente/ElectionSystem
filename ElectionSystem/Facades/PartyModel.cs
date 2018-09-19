50b4f04905a7eeb1271862cba09547f0d6d5f648
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:28:30 +0800

    Created PartyModel.

---
+++ ElectionSystem/Facades/PartyModel.cs
@@ -0,0 +1,36 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class PartyModel : BaseInpc
+    {
+        private readonly Party _party;
+
+        public PartyModel(Party party)
+        {
+            _party = party;
+        }
+
+        public int Id { get => _party.Id; }
+
+        public string Name
+        {
+            get => _party.Name;
+            set
+            {
+                _party.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Logo
+        {
+            get => _party.Logo;
+            set
+            {
+                _party.Logo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
