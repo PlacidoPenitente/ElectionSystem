2a844a790e57d5d3c763e00ab853759e7590ba44
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:25:23 +0800

    Created PositionModel.

---
+++ ElectionSystem/Facades/PositionModel.cs
@@ -0,0 +1,36 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class PositionModel : BaseInpc
+    {
+        private readonly Position _position;
+
+        public PositionModel(Position position)
+        {
+            _position = position;
+        }
+
+        public int Id { get => _position.Id; }
+
+        public string Position
+        {
+            get => _position.Name;
+            set
+            {
+                _position.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Election Election
+        {
+            get => _position.Election;
+            set
+            {
+                _position.Election= value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
