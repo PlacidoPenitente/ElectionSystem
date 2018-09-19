5a1b034da3c093bafc1b7a56d8539f62a50d5ce6
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:16:56 +0800

    Created VoteModel.

---
+++ ElectionSystem/Facades/VoteModel.cs
@@ -0,0 +1,36 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class VoteModel : BaseInpc
+    {
+        private readonly Vote _vote;
+
+        public VoteModel(Vote vote)
+        {
+            _vote = vote;
+        }
+
+        public int Id { get => _vote.Id; }
+
+        public Candidacy Candidacy
+        {
+            get => _vote.Candidacy;
+            set
+            {
+                _vote.Candidacy= value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Ballot Ballot
+        {
+            get => _vote.Ballot;
+            set
+            {
+                _vote.Ballot= value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
