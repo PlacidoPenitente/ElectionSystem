781614aeea98cba58f82fc32f2dada53ceb1486f
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:21:35 +0800

    Created UserModel.

---
+++ ElectionSystem/Facades/UserModel.cs
@@ -0,0 +1,46 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class UserModel : BaseInpc
+    {
+        private readonly User _user;
+
+        public UserModel(User user)
+        {
+            _user = user;
+        }
+
+        public int Id { get => _user.Id; }
+
+        public string Username
+        {
+            get => _user.Username;
+            set
+            {
+                _user.Username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _user.Password;
+            set
+            {
+                _user.Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AccountType AccountType
+        {
+            get => _user.AccountType;
+            set
+            {
+                _user.AccountType = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
