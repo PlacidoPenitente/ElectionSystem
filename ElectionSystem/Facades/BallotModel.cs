9ba835b65bd327243fcbd756b48cdc0887c5b19d
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:42:49 +0800

    Created BallotModel.

---
+++ ElectionSystem/Facades/BallotModel.cs
@@ -0,0 +1,26 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class BallotModel : BaseInpc
+    {
+        private readonly Ballot _ballot;
+
+        public BallotModel(Ballot ballot)
+        {
+            _ballot = ballot;
+        }
+
+        public int Id { get => _ballot.Id; }
+
+        public Voter Voter
+        {
+            get => _ballot.Voter;
+            set
+            {
+                _ballot.Voter = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
