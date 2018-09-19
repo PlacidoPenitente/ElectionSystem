ad098660de52019d7951a50137e82a7314bed702
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-20 02:39:18 +0800

    Created CandidacyModel.

---
+++ ElectionSystem/Facades/CandidacyModel.cs
@@ -0,0 +1,46 @@
+﻿using ElectionSystem.Models;
+
+namespace ElectionSystem.Facades
+{
+    public class CandidacyModel : BaseInpc
+    {
+        private readonly Candidacy _candidacy;
+
+        public CandidacyModel(Candidacy candidacy)
+        {
+            _candidacy = candidacy;
+        }
+
+        public int Id { get => _candidacy.Id; }
+
+        public Voter Candidate
+        {
+            get => _candidacy.Candidate;
+            set
+            {
+                _candidacy.Candidate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Party Party
+        {
+            get => _candidacy.Party;
+            set
+            {
+                _candidacy.Party = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Position Position
+        {
+            get => _candidacy.Position;
+            set
+            {
+                _candidacy.Position = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
