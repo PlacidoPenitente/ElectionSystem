0463595635a211a9128062f135967cd42ae2e375
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-22 14:02:05 +0800

    Completed Register Voters.

---
+++ ElectionSystem/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ElectionSystem.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? DateTime.Now.ToShortDateString() : ((DateTime) value).Date.ToShortDateString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DateTime.Now;
+            try
+            {
+                return DateTime.Parse((string) value);
+            }
+            catch (Exception)
+            {
+                return DateTime.Now;
+            }
+        }
+    }
+}
